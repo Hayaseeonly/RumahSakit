4a01e4b01df84951857adffa5cef9e26b095e786
Hayaseeonly <hafizhandika304@gmail.com>
2025-09-17 22:29:11 +0700

    Tambah Orang.java, update Pasien.java (inheritance, override, super)

---
+++ src/id/ac/polban/model/Orang.java
@@ -0,0 +1,31 @@
+package id.ac.polban.model;
+
+public class Orang {
+    private String nama;
+    private int umur;
+
+    public Orang(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public int getUmur() {
+        return umur;
+    }
+    public void setUmur(int umur) {
+        if (umur > 0) {
+            this.umur = umur;
+        }
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama         : " + nama);
+        System.out.println("Umur         : " + umur + " tahun");
+    }
+}
--- src/id/ac/polban/model/Pasien.java
+++ src/id/ac/polban/model/Pasien.java
@@ -1,35 +1,30 @@
 package id.ac.polban.model;
 
-public class Pasien {
-    private static int jumlahPasien = 0;
+public class Pasien extends Orang {
+   private static int jumlahPasien = 0;
+   private String penyakit;
 
-    private String nama;
-    private int umur;
-    private String penyakit;
+   public Pasien(String nama, int umur, String penyakit) {
+       super(nama, umur);  // panggil konstruktor Orang
+       this.penyakit = penyakit;
+       jumlahPasien++;
+   }
 
-    public Pasien(String nama, int umur, String penyakit) {
-        this.nama = nama;
-        this.umur = umur;
-        this.penyakit = penyakit;
-        jumlahPasien++;
-    }
+   public String getPenyakit() {
+       return penyakit;
+   }
 
-    public String getNama() { return nama; }
-    public void setNama(String nama) { this.nama = nama; }
+   public void setPenyakit(String penyakit) {
+       this.penyakit = penyakit;
+   }
 
-    public int getUmur() { return umur; }
-    public void setUmur(int umur) { if (umur > 0) this.umur = umur; }
+   @Override
+   public void tampilkanInfo() {
+       super.tampilkanInfo();  // dari Orang: nama & umur
+       System.out.println("Penyakit    : " + penyakit);
+   }
 
-    public String getPenyakit() { return penyakit; }
-    public void setPenyakit(String penyakit) { this.penyakit = penyakit; }
-
-    public void tampilkanInfo() {
-        System.out.println("Nama Pasien : " + nama);
-        System.out.println("Umur        : " + umur + " tahun");
-        System.out.println("Penyakit    : " + penyakit);
-    }
-
-    public static int getJumlahPasien() {
-        return jumlahPasien;
-    }
+   public static int getJumlahPasien() {
+       return jumlahPasien;
+   }
 }
